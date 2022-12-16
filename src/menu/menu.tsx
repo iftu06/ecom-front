cf9c750abb666cdf897ca4cb9344ebc0b3dd398a
iftekhar <iftekharuddin52@gmail.com>
2022-12-16 22:50:27 +0600

    menu pages

--- src/menu/menu.tsx
+++ src/menu/menu.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Categories from "../theme/Categories";
+import MenuProducts from "./menu_products";
 
 const Menu = (props: any) => {
-  return <Categories />;
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="col-md-4">
+          <Categories />
+        </div>
+        <div className="col-md-8">
+          <MenuProducts />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Menu;
---
+++ src/menu/menu_products.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Product } from "../product/product";
+import ProductItem from "../product/product-item";
+
+const products: Product[] = [
+  {
+    id: 1,
+    name: "Nokia 3310(2017)",
+    image: "images/new_6.jpg",
+    price: 379,
+    description: "some",
+  },
+  {
+    id: 2,
+    name: "Huawei MediaPad",
+    image: "images/new_8.jpg",
+    price: 225,
+    description: "some",
+  },
+  {
+    id: 3,
+    name: "Huawei MediaPad",
+    image: "images/new_1.jpg",
+    price: 225,
+    description: "some",
+  },
+];
+
+const MenuProducts = (props: any) => {
+  return (
+    <React.Fragment>
+            {products.map((product) => {
+              return (
+                <ProductItem
+                  key={product.id}
+                  id={product.id}
+                  name={product.name}
+                  image={product.image}
+                  price={product.price}
+                />
+              );
+            })}
+    </React.Fragment>
+  );
+};
+
+export default MenuProducts;
